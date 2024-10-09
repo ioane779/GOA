57d035bca27fd61d4460d0529dbf95b7689b782a
ioane779 <Ivakop10@gmail.com>
2024-10-09 19:32:30 +0400

    first commit

---
+++ day 01/homework/hw.py
@@ -0,0 +1,6 @@
+name = "ioane"
+surname = "kopaliani"
+age = 11
+heigth = 150
+favfood = "pizza"
+print("hello my name is " + name + " my surname is " + surname + "my heigth is " + str(heigth) + " my favorite food is " + favfood + "i am  " + str(age) + " years old" )
